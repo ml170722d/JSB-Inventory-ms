62949b36825b78c2b9a768f60bb1e5eb12d58b2b
ml170722d <ml170722.etf@gmail.com>
2023-05-04 00:12:12 +0200

    Edit repository methods

--- src/main/java/rs/hnp/inventory/repositories/ArticleRepository.java
+++ src/main/java/rs/hnp/inventory/repositories/ArticleRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import rs.hnp.inventory.models.Article;
@@ -12,10 +11,7 @@ import rs.hnp.inventory.models.Article;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-  @Query("SELECT a FROM Article a WHERE a.amount > 0")
-  List<Article> findAllAvailable();
-
-  Optional<Article> findByName(String name);
+  List<Article> findByName(String name);
 
   Optional<Article> findByExternalId(String externalId);
 
