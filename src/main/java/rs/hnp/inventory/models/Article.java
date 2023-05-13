8d960acac2fc21a965f570ba72857a88b0e13f6a
ml170722d <ml170722.etf@gmail.com>
2023-05-13 20:03:03 +0200

    Edit article entity

--- src/main/java/rs/hnp/inventory/models/Article.java
+++ src/main/java/rs/hnp/inventory/models/Article.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +19,8 @@ import lombok.NoArgsConstructor;
 public class Article {
   @Id
   @Column(name = "id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @SequenceGenerator(name = "article_id_seq", sequenceName = "article_id_seq", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "article_id_seq")
   private Long id;
 
   @Column(name = "external_id", unique = true)
@@ -30,11 +32,28 @@ public class Article {
   private String unit;
   @Column(name = "amount")
   private Long amount;
-  @Column(name = "description")
+  @Column(name = "description", nullable = true)
   private String description;
 
   @Column(name = "buying_price")
   private Long buying_price;
   @Column(name = "selling_price")
   private Long selling_price;
+
+  public Article(
+      String externalId,
+      String name,
+      String unit,
+      Long amount,
+      String description,
+      Long buying_price,
+      Long selling_price) {
+    this.externalId = externalId;
+    this.name = name;
+    this.unit = unit;
+    this.amount = amount;
+    this.description = description;
+    this.buying_price = buying_price;
+    this.selling_price = selling_price;
+  }
 }
