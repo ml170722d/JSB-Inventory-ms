faf0d41205d04a1d87a9a0fd63eb7234136e8eb0
ml170722d <ml170722.etf@gmail.com>
2023-05-13 20:03:25 +0200

    Add new methods to article repository

--- src/main/java/rs/hnp/inventory/repositories/ArticleRepository.java
+++ src/main/java/rs/hnp/inventory/repositories/ArticleRepository.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import jakarta.transaction.Transactional;
 import rs.hnp.inventory.models.Article;
 
-@Repository
+@Transactional
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
   List<Article> findByName(String name);
 
   Optional<Article> findByExternalId(String externalId);
+
+  boolean existsByExternalId(String id);
+
+  boolean existsByName(String id);
 
 }
