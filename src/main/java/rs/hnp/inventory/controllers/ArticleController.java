9199448747aa4a62163ace86a6f8fff7313e32d1
ml170722d <ml170722.etf@gmail.com>
2023-05-13 20:04:16 +0200

    Edit article contoroller

--- src/main/java/rs/hnp/inventory/controllers/ArticleController.java
+++ src/main/java/rs/hnp/inventory/controllers/ArticleController.java
@@ -1,19 +1,28 @@
 package rs.hnp.inventory.controllers;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import rs.hnp.inventory.dto.article.ArticleDTO;
-import rs.hnp.inventory.dto.article.ArticleUpdateDTO;
-import rs.hnp.inventory.services.ArticleService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import rs.hnp.inventory.dto.article.ArticleDTO;
+import rs.hnp.inventory.exceptions.ApiExceptionFactory;
+import rs.hnp.inventory.services.ArticleService;
+import rs.hnp.inventory.utils.records.ArticleRegistrationRequest;
+import rs.hnp.inventory.utils.records.ArticleUpdateRequest;
 
 @Validated
 @RestController
@@ -24,53 +33,53 @@ public class ArticleController {
   private final ArticleService articleService;
 
   @PostMapping(value = "")
-  public ResponseEntity<ArticleDTO> createArticle(@Valid @RequestBody ArticleDTO article) {
-      return ResponseEntity.ok(articleService.createArticle(article));
+  public ResponseEntity<ArticleDTO> createArticle(@Valid @RequestBody ArticleRegistrationRequest article) {
+    return ResponseEntity.ok(articleService.createArticle(article));
   }
 
   @PostMapping(value = "/multiple")
-  public ResponseEntity<List<ArticleDTO>> createArticles(@Valid @RequestBody List<ArticleDTO> articles) {
-      return ResponseEntity.ok(articleService.createArticles(articles));
+  public ResponseEntity<List<ArticleDTO>> createArticles(
+      @Valid @RequestBody List<ArticleRegistrationRequest> articles) {
+    return ResponseEntity.ok(articleService.createArticles(articles));
   }
 
   @GetMapping(value = "")
   public ResponseEntity<List<ArticleDTO>> getAllArticle() {
-    List<ArticleDTO> list = articleService.findAll();
+    List<ArticleDTO> list = articleService.getAllArticles();
     return ResponseEntity.ok().body(list);
   }
 
   @GetMapping(value = "/available")
   public ResponseEntity<List<ArticleDTO>> getAllAvailableArticle() {
-    List<ArticleDTO> list = articleService.findAllAvailable();
+    List<ArticleDTO> list = articleService.getAllAvailableArticles();
     return ResponseEntity.ok().body(list);
   }
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<ArticleDTO> findById(@PathVariable(name = "id") Long id) {
-    return ResponseEntity.ok().body(articleService.findById(id));
+  public ResponseEntity<ArticleDTO> getById(@PathVariable(name = "id") Long id) {
+    return ResponseEntity.ok().body(articleService.getArticleById(id));
   }
 
   @GetMapping(value = "/name/{name}")
-  public ResponseEntity<List<ArticleDTO>> findByName(@Valid @PathVariable String name) {
-    List<ArticleDTO> list = articleService.findByName(name);
+  public ResponseEntity<List<ArticleDTO>> getByName(@Valid @PathVariable String name) {
+    List<ArticleDTO> list = articleService.getArticleByName(name);
     if (list.isEmpty())
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      throw ApiExceptionFactory.articleNotFound();
     return ResponseEntity.ok().body(list);
   }
 
   @GetMapping(value = "/externalId/{externalId}")
   public ResponseEntity<List<ArticleDTO>> findByExternalId(@Valid @PathVariable String externalId) {
-    List<ArticleDTO> list = articleService.findByExternalId(externalId);
+    List<ArticleDTO> list = articleService.getArticleByExternalId(externalId);
     if (list.isEmpty())
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      throw ApiExceptionFactory.articleNotFound();
     return ResponseEntity.ok().body(list);
   }
 
-  @PutMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+  @PutMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
   public ResponseEntity<ArticleDTO> updateArticle(
-          @Valid @PathVariable Long id,
-          @Valid @RequestBody ArticleUpdateDTO updatedArticle
-  ) {
+      @Valid @PathVariable Long id,
+      @Valid @RequestBody ArticleUpdateRequest updatedArticle) {
     return ResponseEntity.ok(articleService.updateArticle(id, updatedArticle));
   }
 
@@ -79,7 +88,7 @@ public class ArticleController {
     try {
       articleService.deleteArticle(id);
     } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      throw ApiExceptionFactory.articleNotExists();
     }
     return ResponseEntity.ok().build();
   }
@@ -89,7 +98,7 @@ public class ArticleController {
     try {
       articleService.deleteArticle(ids);
     } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      throw ApiExceptionFactory.articleNotExists();
     }
     return ResponseEntity.ok().build();
   }
